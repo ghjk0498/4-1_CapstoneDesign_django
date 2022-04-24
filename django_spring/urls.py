6b44a3388a3f3414e674347105005a3f9c56c333
ghjk0498 <80383559+ghjk0498@users.noreply.github.com>
2022-04-24 19:24:30 +0900

    add anaconda environment

--- django_spring/urls.py
+++ django_spring/urls.py
@@ -17,11 +17,11 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import include, re_path
 
 urlpatterns = [
-    path('', include('for_spring.urls')),
-    path('admin/', admin.site.urls),
+    re_path('', include('for_spring.urls')),
+    re_path('admin/', admin.site.urls),
 ]
 
 if settings.DEBUG:
