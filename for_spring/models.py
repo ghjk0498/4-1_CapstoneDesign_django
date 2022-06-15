eabb5a26f1c88bc7e0ad1a9efde2e73f97f65755
ghjk0498 <80383559+ghjk0498@users.noreply.github.com>
2022-06-15 17:44:02 +0900

    remove path(media) in .gitignore & change model

--- for_spring/models.py
+++ for_spring/models.py
@@ -3,8 +3,8 @@ from django.db import models
 
 # Create your models here.
 class Test(models.Model):
-    id = models.AutoField(primary_key=True)
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    image = models.ImageField(default='default_image.jpeg')
-    csv = models.FileField(default='Anomaly Simulation with tzinfo.csv')
+    version = models.CharField(primary_key=True, max_length=10)
+    # title = models.CharField(max_length=200)
+    # text = models.TextField()
+    # image = models.ImageField(default='default_image.jpeg')
+    csv = models.FileField(default='Anomaly Simulation with tzinfo v0.csv')
